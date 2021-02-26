b224f96d4b61b60be93d07a3f65b2ea8262310b3
jetstream0 <49297268+jetstream0@users.noreply.github.com>
2021-02-25 19:30:05 -0800

    changed color switch to toggle theme

--- htdocs/nav.php
+++ htdocs/nav.php
@@ -1,5 +1,5 @@
 <ul class="nav">
-    <li class="redir"><a href = "#" class = "btn-toggle">Color Switch</a></li>
+    <li class="redir"><a href = "#" class = "btn-toggle">Toggle Theme</a></li>
     <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "changelog"){echo 'active-nav-item';}?>" href="changelog.php">Changelog</a></li>
     <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "leaderboard"){echo 'active-nav-item';}?>" href="leaderboard.php">Leaderboard</a></li>
     <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "61plus"){echo 'active-nav-item';}?>" href="61plus.php">61hz+ Levels</a></li>
@@ -16,7 +16,7 @@
             <a class="nav-buttons <?php if($_SESSION['active-nav'] == "61plus"){echo 'active-nav-item';}?>" href="61plus.php">61hz+ levels</a>
             <a class="nav-buttons <?php if($_SESSION['active-nav'] == "leaderboard"){echo 'active-nav-item';}?>" href="leaderboard.php">Leaderboard</a>
             <a class="nav-buttons <?php if($_SESSION['active-nav'] == "changelog"){echo 'active-nav-item';}?>" href="changelog.php">Changelog</a>
-            <a href="#" class="btn-toggle2">Color Switch</a>
+            <a href="#" class="btn-toggle2">Toggle Theme</a>
         </div>
     </li>
 </ul>
